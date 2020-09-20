51f1177f7ca7d5d97a2a37e221a25484f5d0d353
Christy La Guardia <christy.laguardia@classpass.com>
2020-09-20 12:41:09 -0600

    fix blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,19 @@
 import React from "react";
 import withPageLayout from "../components/Layout/withPageLayout";
 
+// TODO: pageContext is not working
 const Blog = props => {
   // console.log(props);
-  const { pageContext } = props;
+  // const { pageContext } = props;
 
-  if (!pageContext) return <p>Loading...</p>; // TODO:
+  // if (!pageContext) return <p>Loading...</p>; // TODO:
 
-  const { title } = pageContext.node;
+  // const { title } = pageContext.node;
 
   return (
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">Blog</h1>
+      {/* <h1 className="title">{title}</h1> */}
       {/* <p className="subtitle">{date}</p> */}
       {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
       {/* <div dangerouslySetInnerHTML={{ __html: html }} /> */}
